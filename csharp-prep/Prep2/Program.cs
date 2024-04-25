0cc452da74a7bccfff452c97ef3d102e63f94880
margarethdis <margarethdani2021@gmail.com>
2024-04-24 22:03:04 -0600

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Which is you final grade?: ");
+        string grade = Console.ReadLine();
+        int calification = int.Parse(grade);
+
+        string letter = "";
+
+        if (calification >= 90)
+        {
+            letter = "A";
+        }
+        else if (calification >= 80)
+        {
+            letter = "B";
+        }
+        else if (calification >=70)
+        {
+            letter = "C";
+        }
+        else  if (calification >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        {
+            Console.WriteLine($"your grade is {letter}");
+        }
+
+        if (calification >=70)
+        {
+            Console.WriteLine("Congratulaions! You aproved his course!");
+        }
+        else
+        {
+            Console.WriteLine("Seems like you didn't pass this class, but hey, we've all been there. Keep your chin up and keep moving forward.");
+        }
+
+
+        
     }
 }
