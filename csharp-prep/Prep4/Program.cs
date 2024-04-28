1381b31924ce2f2c29bea96aad3af5ebf2c9e544
margarethdis <margarethdani2021@gmail.com>
2024-04-28 00:28:51 -0600

    .

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber; //definimos la varibale out loop , [ara poder utilizarlo despues
+        //si estuvier dentro solo ahi lo podria utilizr]
+
+        //mira el loop ya no necesitas definir la vaiable otra vez
+
+        
+        do
+        {
+            Console.Write("Enter a number: ");
+            string number = Console.ReadLine();
+            userNumber = int.Parse(number);// aqui el programa le asigna el valor de int
+            numbers.Add(userNumber);
+
+        }while(userNumber != 0);
+
+        
+
+
+
+        
+
+        //usar un foreach para iterar la lista hasta que el user
+        //escriba 0
+
+        
+        Console.WriteLine("Added succesfully");
+        Console.Write($"Your list has {numbers.Count} numbers.");
+
+
+
+        
+        
+
+
+
+
+
+
+
     }
 }
